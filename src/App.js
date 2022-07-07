d7b843d544fbd5ecbb2f4404d70cfe2b50b84866
LizzieDworsky <amydworsky15@gmail.com>
2022-07-07 13:27:52 -0600

    created initial axios get request and saved to state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 function App() {
+    const [videoGames, setVideoGames] = useState([]);
+
+    useEffect(() => {
+        getAllGames();
+    }, []);
+
+    async function getAllGames() {
+        let response = await axios.get("http://localhost:8080/all");
+        setVideoGames(response.data);
+    }
+
     return (
         <div>
             <h1>Hello World</h1>
