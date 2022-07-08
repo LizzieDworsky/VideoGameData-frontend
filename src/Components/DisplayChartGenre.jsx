a2274a53f3ad2de4e0c2f8c3f3c28edd401a1949
LizzieDworsky <amydworsky15@gmail.com>
2022-07-08 13:11:23 -0600

    began working on the logic for DisplayChartGenre component

--- src/Components/DisplayChartGenre.jsx
+++ src/Components/DisplayChartGenre.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Chart } from "react-google-charts";
 
 const DisplayChartGenre = ({ array }) => {
-    return <h1>Hello</h1>;
+    let genres = array.map((item) => item.genre);
+    let distinctGenres = [...new Set(genres)];
+    console.log(distinctGenres);
+
+    function generateDataForChart() {
+        const data = [
+            ["Element", "Density", { role: "style" }],
+            ["Copper", 8.94, "#b87333"], // RGB value
+            ["Silver", 10.49, "silver"], // English color name
+            ["Gold", 19.3, "gold"],
+
+            ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
+        ];
+        return data;
+    }
+
+    return (
+        <div>
+            <Chart
+                chartType="ColumnChart"
+                width="100%"
+                height="400px"
+                data={generateDataForChart()}
+            />
+        </div>
+    );
 };
 
 export default DisplayChartGenre;
