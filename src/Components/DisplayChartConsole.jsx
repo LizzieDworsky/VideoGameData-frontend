4c5113de7594d9f3512dfe266213bed909b370c4
LizzieDworsky <amydworsky15@gmail.com>
2022-07-11 10:32:56 -0600

    added distinct publishers to DisplayChartConsole component + found publishers and total global sales for each console + working on adding the correct data to the chart

--- src/Components/DisplayChartConsole.jsx
+++ src/Components/DisplayChartConsole.jsx
@@ -10,27 +10,111 @@ const DisplayChartConsole = ({ array }) => {
     function generateDataForChart() {
         let platforms = array.map((game) => game.platform);
         let distinctPlatforms = [...new Set(platforms)];
+        let publishers = array.map((game) => game.publisher);
+        let distinctPublishers = [...new Set(publishers)];
 
         let platformArrays = distinctPlatforms.map((item) => {
             let allGamesForPlatform = array.filter(
                 (game) => game.platform === item
             );
 
-            let publishers = allGamesForPlatform.map((game) => game.publisher);
-            let distinctPublishers = [...new Set(publishers)];
-            console.log(item, distinctPublishers);
+            let gamePublishers = allGamesForPlatform.map(
+                (game) => game.publisher
+            );
+            let distinctGamePublishers = [...new Set(publishers)];
+            let publisherArray = distinctGamePublishers.map((item) => {
+                let allGamesForPlatformPublisher = allGamesForPlatform.filter(
+                    (game) => game.publisher === item
+                );
+                let total = 0;
+                for (let i = 0; i < allGamesForPlatformPublisher.length; i++) {
+                    total += allGamesForPlatformPublisher[i].globalsales;
+                }
+            });
+            // do the mapping for all games for platform and publisher,
+            // then do a for loop to add those together
 
-            let sum = 0;
-            for (let i = 0; i < allGamesForPlatform.length; i++) {
-                sum += allGamesForPlatform[i].globalsales;
-            }
-            return [item, sum, "#3366CC"];
+            // let sum = 0;
+            // for (let i = 0; i < allGamesForPlatform.length; i++) {
+            //     sum += allGamesForPlatform[i].globalsales;
+            // }
+            return [
+                item,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+            ];
         });
 
-        const data = [
-            ["Platform", "Global Sales in Millions", { role: "style" }],
-            ...platformArrays,
-        ];
+        const data = [["Platform", ...distinctPublishers], ...platformArrays];
         return data;
     }
 
